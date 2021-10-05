829e16e0ed0a7dea9ef9ecfa7257c045d2bdca73
mrkjse <mrk.jse@gmail.com>
2021-10-05 11:17:25 +1100

    add images

--- docker_commands.sh
+++ docker_commands.sh
@@ -1,3 +1,6 @@
+# This is from https://airflow.apache.org/docs/apache-airflow/stable/start/docker.html
+# The link above contains info on how to install Docker and Apache Airflow
+
 # Initial run
 # Retrieve Airflow Docker YAML file
 curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.1.4/docker-compose.yaml'
@@ -12,9 +15,9 @@ AIRFLOW_GID=0
 
 
 # BAU runs can start from here
-# This is from https://airflow.apache.org/docs/apache-airflow/stable/start/docker.html
 docker-compose up airflow-init
 docker-compose up
+# Go to localhost:8080
 # Login uid: airflow pwd: airflow
 
 # Resetting the Docker environment
